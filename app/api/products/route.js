da090122bf6a0a5bc24e60f62228cd723fdfb7d7
NaveenkumarSimpleDev <128913146+NaveeenkumarSimpleDev@users.noreply.github.com>
2024-01-01 17:49:29 +0000

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,8 +2,7 @@ import { getPrismaClient } from "@/provider/prismadb";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-// // const prisma = await getPrismaClient();
+const prisma = await getPrismaClient();
 
 export async function GET(req) {
   const { nextUrl } = await req;
@@ -20,32 +19,27 @@ export async function GET(req) {
         }
       : sort === "priceLowToHigh"
         ? {
-            discountPrice: "desc",
+            discountPrice: "asc",
           }
         : sort === "priceHighToLow"
           ? {
-              discountPrice: "asc",
+              discountPrice: "desc",
             }
           : false;
   let whereCondition = {};
 
   if (range && range?.length > 1) {
-    const minPrice = parseInt(range[0] || 0);
-    const maxPrice = parseInt(range[1] || 0);
-    if (minPrice && maxPrice != 0) {
-      whereCondition = {
-        variations: {
-          some: {
-            price: {
-              gte: minPrice,
-              lte: maxPrice,
-            },
-          },
-        },
-      };
-    }
+    const minPrice = parseInt(range[0]);
+    const maxPrice = parseInt(range[1]);
+
+    whereCondition = {
+      discountPrice: {
+        gte: minPrice,
+        lte: maxPrice,
+      },
+    };
   }
-  if (categories.length > 0) {
+  if (categories?.length > 0) {
     whereCondition = {
       ...whereCondition,
       category: {
@@ -53,175 +47,28 @@ export async function GET(req) {
       },
     };
   }
-  console.log(whereCondition);
-  const products = await prisma.product.findMany({
-    include: {
-      variations: true,
-    },
-    where: whereCondition,
-    orderBy: false,
-  });
+
+  let condition;
+  if (sortBy) {
+    condition = {
+      include: {
+        variations: true,
+      },
+      where: whereCondition,
+      orderBy: sortBy,
+    };
+  } else {
+    condition = {
+      include: {
+        variations: true,
+      },
+      where: whereCondition,
+    };
+  }
+
+  const products = await prisma.product.findMany(condition);
 
   return NextResponse.json(products, {
     status: 200,
   });
-
-  // if(brands.length > 0){
-  //   whereCondition = {
-  //     ...whereCondition,
-  //     brand: {
-  //       name: {
-  //         in: brands
-  //       }
-  //     }
-  //   }
-  // }
 }
-
-// // export async function GET(req) {
-// //   const { nextUrl } = await req;
-
-// //   const pr = await prisma.product.findMany({
-// //     include: {
-// //       brand: true,
-// //     },
-// //   });
-// //   return NextResponse.json(pr, {
-// //     status: 200,
-// //   });
-// //   const sort = nextUrl.searchParams.get("sort");
-// //   const range = nextUrl.searchParams.get("price_range")?.split("-");
-// //   const categories = nextUrl.searchParams.get("cat")?.split(",");
-// //   const brands = nextUrl.searchParams.get("brands")?.split(",");
-
-// //   let whereCondition;
-
-// //   if (range[0] && range[1]) {
-// //     const minPrice = parseInt(range[0]);
-// //     const maxPrice = parseInt(range[1]);
-// //     if (minPrice && maxPrice != 0) {
-// //       whereCondition = {
-// //         variations: {
-// //           some: {
-// //             price: {
-// //               gte: minPrice,
-// //               lte: maxPrice,
-// //             },
-// //           },
-// //         },
-// //       };
-// //     }
-// //   }
-
-// //   if (categories.length > 0) {
-// //     whereCondition.category = {
-// //       in: categories,
-// //     };
-// //   }
-
-// //   if (brands.length > 0) {
-// //     whereCondition.brand = {
-// //       in: brands,
-// //     };
-// //   }
-
-// //   const filteredProducts = await prisma.product.findMany({
-// //     where: whereCondition,
-// //     include: {
-// //       variations: true,
-// //       brand: true,
-// //     },
-// //     orderBy: {
-// //       category: "asc",
-// //       brand: "asc",
-// //     },
-// //   });
-
-// //   return NextResponse.json(filteredProducts, {
-// //     status: 200,
-// //   });
-
-// //   if (sort === "Newest") {
-// //     const products = await prisma.product.findMany({
-// //       include: {
-// //         brand: true,
-// //       },
-// //       orderBy: [
-// //         {
-// //           createdAt: "desc",
-// //         },
-// //       ],
-// //     });
-
-// //     let filteredProducts;
-// //     if (categories) {
-// //       filteredProducts = products.filter((product) =>
-// //         categories.includes(product.category),
-// //       );
-// //     } else {
-// //       filteredProducts = [...products];
-// //     }
-// //     if (range) {
-// //       filteredProducts = filteredProducts.filter(
-// //         (p) =>
-// //           Number(p.discountPrice) > Number(range[0]) &&
-// //           Number(p.discountPrice) < Number(range[1]),
-// //       );
-// //     }
-
-// //     if (brands) {
-// //       filteredProducts = filteredProducts.filter((p) =>
-// //         brands.includes(p.brand.value),
-// //       );
-// //     }
-
-// //     return NextResponse.json(filteredProducts, {
-// //       status: 200,
-// //     });
-// //   }
-
-// //   if (sort === "priceLowToHigh" || sort === "priceHighToLow") {
-// //     const products = await prisma.product.findMany({
-// //       include: {
-// //         brand: true,
-// //       },
-// //       orderBy: [
-// //         {
-// //           discountPrice: sort === "priceLowToHigh" ? "asc" : "desc",
-// //         },
-// //       ],
-// //     });
-
-// //     let filteredProducts;
-// //     if (categories) {
-// //       filteredProducts = products.filter((product) =>
-// //         categories.includes(product.category),
-// //       );
-// //     } else {
-// //       filteredProducts = [...products];
-// //     }
-// //     if (range) {
-// //       filteredProducts = filteredProducts.filter(
-// //         (p) =>
-// //           Number(p.discountPrice) > Number(range[0]) &&
-// //           Number(p.discountPrice) < Number(range[1]),
-// //       );
-// //     }
-
-// //     if (brands) {
-// //       filteredProducts = filteredProducts.filter((p) =>
-// //         brands.includes(p.brand.value),
-// //       );
-// //     }
-
-// //     return NextResponse.json(filteredProducts, {
-// //       status: 200,
-// //     });
-// //   }
-
-// //   const products = await prisma.product.findMany();
-
-// //   return NextResponse.json(products, {
-// //     status: 200,
-// //   });
-// // }
